c53a14307db99a49051c66c19098687165bb7000
ananicolas <134564005+ananicolas@users.noreply.github.com>
2024-10-08 11:50:15 -0300

    ytrfg

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Matemática',
+    'Como monta uma equação do 3° grau?',
+    'ax³+bx²+cx+d=0, com a ≠ 0 e raízes x1, x2 e x3'
+)
+
+criaCartao(
+    'Matemática',
+    'Em uma equação, qual é a ordem certa para realizar a conta?',
+    'parênteses, expoentes, multiplicação e divisão (mesmo nível) e adição e subtração (mesmo nível).'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 56 divido por 7?',
+    '8'
+)
+
+criaCartao(
+    'Matemática',
+    'O PI é um número racional ou irracional?',
+    'Irracional'
+)
+
+criaCartao(
+    'Matemática',
+    'Como também é chamado o perímetro de um círculo?',
+    'Circunferência'
+)
