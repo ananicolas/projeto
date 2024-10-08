88d2517cbacf79ef7be25223823fccaab39ef298
ananicolas <134564005+ananicolas@users.noreply.github.com>
2024-10-08 12:24:09 -0300

    rteyg

--- perguntas.js
+++ perguntas.js
@@ -27,3 +27,83 @@ criaCartao(
     'Como também é chamado o perímetro de um círculo?',
     'Circunferência'
 )
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral'
+)
+
+criaCartao(
+    'História',
+    'Qual era a importância dos faraós?',
+    'O faraó era responsável pela condução das atividades agrícolas e obras públicas de toda nação.'
+)
+
+criaCartao(
+    'História',
+    'Quais eram os principais ditadores do século XX?',
+    'Adolf Hitler; Josef Stalin; Benito Mussolini; Mao Zedong.'
+)
+
+criaCartao(
+    'História',
+    'Quais foram as duas potências que disputavam no contexto da Guerra Fria?',
+    'Os Estados Unidos e a União Soviética'
+)
+
+criaCartao(
+    'História',
+    'Quais eram os países do Eixo?',
+    'Alemanha, Italia e Japão'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'Dois átomos de Hidrogênio e um átomo de oxigênio (H2O)'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma ligação iônica?',
+    'São as ligações químicas que ocorrem, normalmente, entre metais e ametais.'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma ligação covalente?',
+    'São ligações químicas formadas pelo compartilhamento de elétrons de valência entre dois átomos.'
+)
+
+criaCartao(
+    'Química',
+    'Quais são as principais funções oxigenadas?',
+    'O álcool, o Enol e o Fenol'
+)
+
+criaCartao(
+    'Química',
+    'O que são transformações endotérmicas?',
+    'Aquelas que absorvem o calor'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a camada mais profunda e quente da Terra?',
+    'O Núcleo interno'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o tipo de limite de placas tectônicas onde duas placas se afastam uma da outra?',
+    'Limite divergente'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os principais tipos de energia renováveis?',
+    'Energia Eólica, Energia Solar e Energia Hidráulica.'
+)
+
+criaCartao()
