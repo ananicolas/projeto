0f6449a375c0cea9b762595194e44193142f5619
ananicolas <134564005+ananicolas@users.noreply.github.com>
2024-11-06 11:46:05 -0300

    cv

--- pagina2.js
+++ pagina2.js
@@ -18,11 +18,11 @@ function criaCartao (categoria, pergunta, resposta){
 }
 
 function mostraResposta(card){
-    const resposta =card.querySelector('.resposta-cartao')
+    const resposta =card.querySelector('.resposta-cartao');
     if(resposta.style.display === 'none'){
-       resposta.style.display = 'block'
+       resposta.style.display = 'block';
     }
     else{
-       resposta.style.display = 'none'
+       resposta.style.display = 'none';
     }
 }
